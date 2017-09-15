c1138827a744f578934dfc0c50f6beddf17b98f6
Michael Rodgers <mrodgers@appriss.com>
2017-09-15 11:42:32 -0400

    I have been slowly refactoring this main class to move my ideas of how this chord-parser library should work out to differnt factory, producer and loader classes.

--- src/main/java/com/mdrsolutions/chordpro/parser/Main.java
+++ src/main/java/com/mdrsolutions/chordpro/parser/Main.java
@@ -2,11 +2,10 @@ package com.mdrsolutions.chordpro.parser;
 
 import com.mdrsolutions.chordpro.parser.factories.ChordLocationProducer;
 import com.mdrsolutions.chordpro.parser.factories.DirectiveLocationProducer;
+import com.mdrsolutions.chordpro.parser.factories.SimpleTextLineProducer;
 import com.mdrsolutions.chordpro.parser.factories.SongLineProducer;
-import com.mdrsolutions.chordpro.parser.models.DirectiveLocation;
-import com.mdrsolutions.chordpro.parser.models.ArrangedSongLine;
+import com.mdrsolutions.chordpro.parser.models.SimpleTextSongLine;
 import com.mdrsolutions.chordpro.parser.models.RawSongLine;
-import com.mdrsolutions.chordpro.parser.models.ChordLocation;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,13 +15,7 @@ import java.util.List;
  * @author mrodgers
  */
 public class Main {
-
-    private static final String EMPTY_SPACE = "";
-    private static final String SINGLE_SPACE = " ";
-    private static final String OPEN_SQUARE_BRACKET = "[";
-    private static final String CLOSE_SQUARE_BRACKET = "]";
-    private static final String CHORD_PRO_CHORD_REGEX_PATTERN = "\\[(.*?)\\]";
-    private static final String CHORD_PRO_DIRECTIVE_REGEX_PATTERN = "\\{(.*?)\\}";
+ 
     private static final String SONG
             = "{c: Verse 1: }Bless the[C] Lord, O my[G] soul,[D/F#] O my [Em] soul,\n"
             + "[C]Worship His ho[G]ly n[Dsus4]ame.    [D]\n"
@@ -33,10 +26,12 @@ public class Main {
     public static void main(String[] args) {
         
         SongLineProducer lineProducer = new SongLineProducer(new ChordLocationProducer(), new DirectiveLocationProducer());
+        SimpleTextLineProducer textLineProducer = new SimpleTextLineProducer();
+        
         System.out.println("num = " + num);
         System.out.println(SONG);
         List<RawSongLine> songLines = new LinkedList<>();
-        List<ArrangedSongLine> arrangedSongLines = new LinkedList<>();
+        List<SimpleTextSongLine> arrangedSongLines = new LinkedList<>();
         
         //identify song line chord components
         songLines(SONG).forEach((songLine) -> {
@@ -45,7 +40,7 @@ public class Main {
 
         //transform Song lines
         songLines.forEach((line) -> {
-            arrangedSongLines.add(transformSongLine(line));
+            arrangedSongLines.add(textLineProducer.produce(line));
         });
         
         //print Arranged Chords and Song Lines
@@ -61,50 +56,6 @@ public class Main {
         String[] split = song.split("\\n");
         songLines.addAll(Arrays.asList(split));
         return songLines;
-    } 
-
-    private static ArrangedSongLine transformSongLine(RawSongLine rawSongLine) {
-        List<ChordLocation> chordLocations = rawSongLine.getChordLocations();
-        String songLine = rawSongLine.getSong(); 
-        
-        StringBuilder chordLocaleBldr = new StringBuilder();
-        Integer previousColumnIndex = 0;
-        
-        for(ChordLocation chordLocation : chordLocations){
-            
-            Integer columnIndex = chordLocation.getColumnIndex();
-            columnIndex = (columnIndex == 0 ? 0 : columnIndex -1);
-            String bracketChord = chordLocation.getChord();
-            Integer bracketChordLength = bracketChord.length();
-            
-            String debracketedChord = debracket(bracketChord);
-            
-            chordLocaleBldr.append(addSpaces(previousColumnIndex, columnIndex)).append(debracketedChord);            
-            //the new startColumnIndex as been reset to the current columnIndex
-            previousColumnIndex = columnIndex + bracketChordLength +1;
-        }
-        
-        songLine = songLine.replaceAll(CHORD_PRO_CHORD_REGEX_PATTERN, "");
-        System.out.println(chordLocaleBldr.toString());
-        System.out.println(songLine);
-        
-        return new ArrangedSongLine(chordLocaleBldr.toString(), songLine);
-    }
+    }  
     
-    private static String debracket(String chord){
-        return chord.replace(OPEN_SQUARE_BRACKET, EMPTY_SPACE).replace(CLOSE_SQUARE_BRACKET, EMPTY_SPACE);
-    }
-    
-    private static String addSpaces(Integer previousColumnIndex, Integer startPositionColumn){
-        String spaces = EMPTY_SPACE;
-        if(startPositionColumn == 0){
-            return spaces;
-        } else {
-            startPositionColumn = startPositionColumn - previousColumnIndex;
-            for(int i = 0; i < startPositionColumn; i++){
-                spaces = spaces + SINGLE_SPACE;
-            }
-        }
-        return spaces;
-    }
 }
