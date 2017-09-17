7093cf98558f1009b22e3ba808f37f8e9e8f4660
Michael Rodgers <mrodgers@appriss.com>
2017-09-16 22:08:57 -0400

    Had to update the chordLocationProducer to eliminate directives when counting out spaces.

--- src/main/java/com/mdrsolutions/chordpro/parser/factories/ChordLocationProducer.java
+++ src/main/java/com/mdrsolutions/chordpro/parser/factories/ChordLocationProducer.java
@@ -15,16 +15,20 @@ import java.util.regex.Pattern;
 public class ChordLocationProducer<LO extends ChordLocation> implements LocationProducer<LO> {
 
     private static final String CHORD_PRO_CHORD_REGEX_PATTERN = "\\[(.*?)\\]";
+    private static final String META_TAGS_DIRECT_REGEX_PATTERN = "\\{.:(.*?)\\}";
+    private static final String SECTIONAL_DIRECT_REGEX_PATTERN = "\\{(.*?)\\}.*?\\{(.*?)\\}";
     
     @Override
     public List<Location> produce(String songLine) {
         Pattern pattern = Pattern.compile(CHORD_PRO_CHORD_REGEX_PATTERN);
         Matcher matcher = pattern.matcher(songLine); 
         List<Location> chords = new LinkedList<>();
-
+        
+        songLine = eliminateDirectiveWrappers(songLine);
+        
         int chordLocation = 0;
         while (matcher.find()) {
-            String rawChord = matcher.group(0);
+            String rawChord = matcher.group(0); 
             chordLocation = songLine.indexOf(rawChord, chordLocation);
             chords.add(new ChordLocation(rawChord, chordLocation));
             
@@ -32,6 +36,26 @@ public class ChordLocationProducer<LO extends ChordLocation> implements Location
             chordLocation++;
         } 
         return chords;
+    }
+    
+    private String eliminateDirectiveWrappers(String songLine){
+        Matcher metaTagDirectMatcher = Pattern.compile(META_TAGS_DIRECT_REGEX_PATTERN).matcher(songLine); 
+        Matcher sectionalDirectMatcher = Pattern.compile(SECTIONAL_DIRECT_REGEX_PATTERN).matcher(songLine); 
+        
+        while(metaTagDirectMatcher.find()){
+            String metaOriginal = metaTagDirectMatcher.group(0);
+            String metaWithout = metaTagDirectMatcher.group(1); 
+            songLine = songLine.replace(metaOriginal, metaWithout);
+        }
+        
+        while(sectionalDirectMatcher.find()){
+            String sectWithoutStart = sectionalDirectMatcher.group(1); 
+            String sectWithoutEnd = sectionalDirectMatcher.group(2); 
+            songLine = songLine.replace("{"+sectWithoutStart+"}","").replace("{"+sectWithoutEnd+"}", "");
+        }
+        
+        
+        return songLine;
     }
     
 }
