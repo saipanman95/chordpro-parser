3e9e6e655b71d55b61eb7c75220ce6eefdb3231b
Michael Rodgers <mrodgers@appriss.com>
2017-09-16 22:09:43 -0400

    updated main class to reflect ongoing evolution of this parser

--- src/main/java/com/mdrsolutions/chordpro/parser/Main.java
+++ src/main/java/com/mdrsolutions/chordpro/parser/Main.java
@@ -2,7 +2,9 @@ package com.mdrsolutions.chordpro.parser;
 
 import com.mdrsolutions.chordpro.parser.factories.ChordLocationProducer;
 import com.mdrsolutions.chordpro.parser.factories.DirectiveLocationProducer;
+import com.mdrsolutions.chordpro.parser.factories.Loader;
 import com.mdrsolutions.chordpro.parser.factories.SimpleTextLineProducer;
+import com.mdrsolutions.chordpro.parser.factories.SongFileLoader;
 import com.mdrsolutions.chordpro.parser.factories.SongLineProducer;
 import com.mdrsolutions.chordpro.parser.models.SimpleTextSongLine;
 import com.mdrsolutions.chordpro.parser.models.RawSongLine;
@@ -17,26 +19,21 @@ import java.util.List;
 public class Main {
  
     private static final String SONG
-            = "{c: Verse 1: }Bless the[C] Lord, O my[G] soul,[D/F#] O my [Em] soul,\n"
+            = "{c: Verse 1: }\n"
+            + "{soi}(das){eoi} Bless the[C] Lord, O my[G] {soh}(now dance){eoh}soul,[D/F#] O my [Em] soul,\n"
             + "[C]Worship His ho[G]ly n[Dsus4]ame.    [D]\n"
             + "Sing like [C]never be[Em]fore, [C]   [D]O m[Em]y soul.\n"
             + "I'll[C] worship Your ho[D]ly na[C/G]me.   [G]";
-   static int num= 0;
-   
+    
     public static void main(String[] args) {
         
         SongLineProducer lineProducer = new SongLineProducer(new ChordLocationProducer(), new DirectiveLocationProducer());
+        Loader<List<RawSongLine>, String, SongLineProducer> loader = new SongFileLoader(lineProducer);
         SimpleTextLineProducer textLineProducer = new SimpleTextLineProducer();
-        
-        System.out.println("num = " + num);
-        System.out.println(SONG);
-        List<RawSongLine> songLines = new LinkedList<>();
+           
         List<SimpleTextSongLine> arrangedSongLines = new LinkedList<>();
         
-        //identify song line chord components
-        songLines(SONG).forEach((songLine) -> {
-            songLines.add(lineProducer.produce(songLine));
-        });
+        List<RawSongLine> songLines = loader.load("/Users/mrodgers/songfiles/testsong.chordpro");
 
         //transform Song lines
         songLines.forEach((line) -> {
@@ -46,16 +43,7 @@ public class Main {
         //print Arranged Chords and Song Lines
         arrangedSongLines.forEach((line) -> {
             System.out.println(line);
-        });
-        num = num +1;
-        System.out.println("num = " + num);
-    }
-
-    private static List<String> songLines(String song) {
-        List<String> songLines = new LinkedList<>();
-        String[] split = song.split("\\n");
-        songLines.addAll(Arrays.asList(split));
-        return songLines;
-    }  
+        }); 
+    } 
     
 }
