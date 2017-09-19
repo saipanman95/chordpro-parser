7b77b689ca6c67d9e51a55d4b1875748fc2166a2
Michael Rodgers <mrodgers@appriss.com>
2017-09-18 20:56:29 -0400

    continuing to modify the main method as I test this project

--- src/main/java/com/mdrsolutions/chordpro/parser/Main.java
+++ src/main/java/com/mdrsolutions/chordpro/parser/Main.java
@@ -5,11 +5,13 @@ import com.mdrsolutions.chordpro.parser.factories.producers.DirectiveLocationPro
 import com.mdrsolutions.chordpro.parser.factories.loaders.Loader;
 import com.mdrsolutions.chordpro.parser.factories.producers.SimpleTextLineProducer;
 import com.mdrsolutions.chordpro.parser.factories.loaders.SongFileLoader;
+import com.mdrsolutions.chordpro.parser.factories.processor.Processor;
+import com.mdrsolutions.chordpro.parser.factories.processor.SongProcessor;
+import com.mdrsolutions.chordpro.parser.factories.producers.Producer;
 import com.mdrsolutions.chordpro.parser.factories.producers.SongLineProducer;
 import com.mdrsolutions.chordpro.parser.models.SimpleTextSongLine;
 import com.mdrsolutions.chordpro.parser.models.RawSongLine;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -17,33 +19,30 @@ import java.util.List;
  * @author mrodgers
  */
 public class Main {
- 
+
+    private static final String PATH = "/Users/mrodgers/songfiles/testsong.chordpro";
     private static final String SONG
             = "{c: Verse 1: }\n"
             + "{soi}(das){eoi} Bless the[C] Lord, O my[G] {soh}(now dance){eoh}soul,[D/F#] O my [Em] soul,\n"
             + "[C]Worship His ho[G]ly n[Dsus4]ame.    [D]\n"
             + "Sing like [C]never be[Em]fore, [C]   [D]O m[Em]y soul.\n"
             + "I'll[C] worship Your ho[D]ly na[C/G]me.   [G]";
-    
-    public static void main(String[] args) {
-        
-        SongLineProducer lineProducer = new SongLineProducer(new ChordLocationProducer(), new DirectiveLocationProducer());
-        Loader<List<RawSongLine>, String, SongLineProducer> loader = new SongFileLoader(lineProducer);
-        SimpleTextLineProducer textLineProducer = new SimpleTextLineProducer();
-           
-        List<SimpleTextSongLine> arrangedSongLines = new LinkedList<>();
-        
-        List<RawSongLine> songLines = loader.load("/Users/mrodgers/songfiles/testsong.chordpro");
 
-        //transform Song lines
-        songLines.forEach((line) -> {
-            arrangedSongLines.add(textLineProducer.produce(line));
-        });
-        
-        //print Arranged Chords and Song Lines
-        arrangedSongLines.forEach((line) -> {
-            System.out.println(line);
-        }); 
-    } 
-    
+    public static void main(String[] args) {
+
+        Producer rawSongLineProducer = new SongLineProducer(new ChordLocationProducer(), new DirectiveLocationProducer());
+
+        Loader<List<RawSongLine>> loader = new SongFileLoader(PATH, rawSongLineProducer);
+
+        Producer<SimpleTextSongLine, RawSongLine> simpleTextLineProducer = new SimpleTextLineProducer();
+
+        Processor<List<RawSongLine>, SimpleTextSongLine, RawSongLine> songProcessor = new SongProcessor();
+
+        Collection<SimpleTextSongLine> song = songProcessor.process(loader, simpleTextLineProducer);
+
+        for(SimpleTextSongLine tsl : song){
+            System.out.println(tsl);
+        }
+    }
+
 }
